fe5deb00ee0560578d3a1d9ce44867f6ff42793a
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2022-05-20 08:32:47 +0700

    update

---
+++ Assets/resourceTextController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class resourceTextController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text anorganic;
+    public Text organic;
+    public Text electric;
+    public Text stone;
+    public Text water;
+
+    public Text dayCounter;
+    public Text timeCounter;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
