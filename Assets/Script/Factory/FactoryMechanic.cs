4010de4ce7ca260d1f3fafc36489e275f78376db
Rizqial5 <bagusriski1@gmail.com>
2022-06-09 18:20:14 +0700

    Add Factory Mechanic
    - Adding factory mechanic that can input material then produce new material
    - Modify Inventory Data scriptable object to adjust this mechanic
    - Modify buildings prefab
    - Add Factory Panel for factory UI
    - Add FactoryMechanic script and FactoryPanelUI script

--- Assets/Script/Factory/FactoryMechanic.cs
+++ Assets/Script/Factory/FactoryMechanic.cs
@@ -1,18 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rehorizon.Core;
+using Rehorizon.Stats;
+using Rehorizon.Inventory;
+using UnityEngine.UI;
 
-public class FactoryMechanic : MonoBehaviour
+namespace Rehorizon.Factory
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FactoryMechanic : MonoBehaviour
     {
+
+        [SerializeField] FactoryPanelUI factoryPanel;
+        [SerializeField] BuildingType buildingType;
+        [SerializeField] BuildingStats buildingStats;
+        [SerializeField] InventoryData inventoryData;
+
+        private Dictionary<StatsType, int> inputMaterial;
+        private int finalValue;
+        
+
+        bool isPanelShow;
+        // Start is called before the first frame update
+
+        private void Awake() 
+        {
+            isPanelShow = false;
+        }
+
+
+
+        private void OnMouseDown() 
+        {
+            if(GridBuilding.current.GetBuildMode()) return;
+
+            isPanelShow = !isPanelShow;
+            factoryPanel.gameObject.SetActive(isPanelShow);
+            
+        }
+
+        private Dictionary<StatsType, int> ShowMaterial()
+        {
+            Dictionary<StatsType, int> materials = buildingStats.GetInputMaterial(buildingType);
+            
+            return materials;
+        }
+
+        
+
+        public StatsType GetMaterialName()
+        {
+            inputMaterial = ShowMaterial();
+            
+            StatsType materialName;
+
+            foreach (var item in inputMaterial)
+            {
+                materialName = item.Key;
+                return materialName;
+            }
+
+            return StatsType.Baterai ;
+
+        }
+
+        public int GetMaterialValue()
+        {
+            inputMaterial = ShowMaterial();
+            
+            int materialValue;
+
+            foreach (var item in inputMaterial)
+            {
+                materialValue = item.Value;
+                return materialValue;
+            }
+
+            return 0 ;
+
+        }
+
+        public int GetResourceAvailable()
+        {
+            return inventoryData.GetAmountInventory(GetMaterialName());
+        }
+
+        public BuildingType GetBuildingType()
+        {
+            return buildingType;
+        }
+
+
+
+        public bool GetIsPanelShow()
+        {
+            return isPanelShow;
+        }
+
+        public void SetResource(int amount)
+        {
+            inventoryData.SetAmountInventory(GetMaterialName(),amount);
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
